48975567df8f433f9298235d333eb9ab57425537
Kirti <162712321+Kirti391@users.noreply.github.com>
2026-09-20 22:54:58 +0530

    Props practiced: sweet shop

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 import Card from './card';
+import Navbar from './Navbar';
+import './App.css'
 function App() {
-  return (
-    //react fragments 
-    <>
-    <div><h1>Hi</h1></div>
-    <div>
-      <h2>Hello!</h2>
+  
     
-    </div>
-     <Card/>
-     <Card/>
-     <Card/>
-    </>
+  return (
+
+    //react fragments 
+  
+  //  <div>
+  //   <h1>Hello Guys, I am {user}</h1>
+  //   <p>
+  //     I am {age} years old
+  //   </p>
+
+  //  </div>
+  <> <div>
+    <Navbar/>
+
+  
+    
+  </div>
+  <div className='card-container'>
+  <Card name="Gulab Jamun" description="Soft and sweet gulab jamun soaked in fragrant sugar syrup" image="https://th.bing.com/th/id/OIP.RyIRoXVi_u8k4xdMnNx3KAHaHa?w=162&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3z"/>
+     <Card name="Rasgulla" description="Soft and spongy cottage cheese balls in a sweet syrup" image="https://img.freepik.com/premium-photo/rasgulla-is-one-famous-indian-sweet-made-by-pure-cow-milk-this-sweet-is-served-chilled-has-softspongy-texure-with-divine-aroma-cardamom-saffron_57665-15409.jpg?w=1380"/>
+     <Card name="Jalebi" description="Crispy and sweet spiral-shaped dessert soaked in sugar syrup" image="https://foodaazz.com/wp-content/uploads/2023/11/Jalebi-1.png"/>
+      <Card name="Ladoo" description="Round, sweet balls made from gram flour and sugar" image="https://th.bing.com/th?q=Motichur&w=120&h=120&c=1&rs=1&qlt=70&r=0&o=7&cb=1&dpr=1.3&pid=InlineBlock&rm=3&mkt=en-IN&cc=IN&setlang=en&adlt=strict&t=1&mw=247"/>
+       <Card name="Barfi" description="Rich and creamy milk-based sweet with nuts" image="https://th.bing.com/th?q=Plain+Barfi&w=120&h=120&c=1&rs=1&qlt=70&r=0&o=7&cb=1&dpr=1.3&pid=InlineBlock&rm=3&mkt=en-IN&cc=IN&setlang=en&adlt=strict&t=1&mw=247"/>
+  </div>
+ </>
   )
 }
 
--- src/card.jsx
+++ src/card.jsx
@@ -1,16 +1,27 @@
 import "./card.css"
+function Card({image, name,description}){
+    const user="Kirti";
+    const age=89;
+   return (
+  <div className="card-container">
+   {/* <div className="card">
+    <h2>Hello Guys, I am {user}</h2>
+    <p>
+      I am {age} years old
+    </p>
 
-function Card(){
-const user="Kirti";
-const age=21;
-    return (
-        <div className="card"> 
-            <h2>Hello !</h2>
-        <p>Hi, I am {user} and I am {age} years old</p>
-       
-        </div>
-        
-    )
+   </div> */}
 
+   <div className="card2">
+    <img src={image} alt={name} />
+    <h2>
+        {/* Gulab Jamun */}
+        {name}
+    </h2>
+    <p>{description}</p>
+    <button>Order Now</button>
+   </div>
+   </div>
+  )
 }
 export default Card;
